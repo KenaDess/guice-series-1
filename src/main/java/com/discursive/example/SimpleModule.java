67daf97d2b2b2b5017e298c1d0bfc1836cb6ae5e
Pauline Dessingue <dessingue.pauline@gmail.com>
2015-11-14 17:25:29 +0100

    Add some examples of annotated class fields

--- src/main/java/com/discursive/example/SimpleModule.java
+++ src/main/java/com/discursive/example/SimpleModule.java
@@ -2,17 +2,24 @@ package com.discursive.example;
 
 import com.discursive.example.student.Registrar;
 import com.discursive.example.student.StudentStore;
+import com.discursive.example.student.exemple.ExempleImpl;
+import com.discursive.example.student.exemple.ExempleItf;
 import com.discursive.example.student.register.LenientRegistrar;
 import com.discursive.example.student.store.DummyStudentStore;
 import com.google.inject.AbstractModule;
 
 public class SimpleModule extends AbstractModule {
 
-	@Override
-	protected void configure() {
-		
-		bind(StudentStore.class).to(DummyStudentStore.class);
-		bind(Registrar.class).to(LenientRegistrar.class);
-	}
+  @Override
+  protected void configure() {
 
+    bind(StudentStore.class).to(DummyStudentStore.class);
+    bind(Registrar.class).to(LenientRegistrar.class);
+    bind(String.class).toInstance("Test");
+    bind(ExempleItf.class).toInstance(getExempleInstance());
+  }
+
+  private ExempleImpl getExempleInstance() {
+    return new ExempleImpl();
+  }
 }
---
+++ src/main/java/com/discursive/example/student/exemple/ExempleItf.java
@@ -0,0 +1,5 @@
+package com.discursive.example.student.exemple;
+
+public interface ExempleItf {
+
+}
