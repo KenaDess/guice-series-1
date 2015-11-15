9eaed8cdf0b1b4f5a6ea91272c68719ef5dc38dd
Pauline Dessingue <dessingue.pauline@gmail.com>
2015-11-15 18:45:05 +0100

    Modification SimpleModule

--- src/main/java/com/discursive/example/SimpleModule.java
+++ src/main/java/com/discursive/example/SimpleModule.java
@@ -12,19 +12,20 @@ import com.google.inject.name.Names;
 
 public class SimpleModule extends AbstractModule {
 
-	@Override
-	protected void configure() {
-		
-		bind(StudentStore.class).toInstance(gtClass());
-		bind(Registrar.class).to(LenientRegistrar.class);
-		bind(String.class).toInstance("Test");
-	    bind(ExempleItf.class).toInstance(getExempleInstance());
-	}
-	
-	public DummyStudentStore gtClass(){
-		return new DummyStudentStore();
-	}
-	private ExempleImpl getExempleInstance() {
-	    return new ExempleImpl();
-	  }
+  @Override
+  protected void configure() {
+
+    bind(StudentStore.class).toInstance(gtClass());
+    bind(Registrar.class).to(LenientRegistrar.class);
+    bind(String.class).toInstance("Test");
+    bind(ExempleItf.class).toInstance(getExempleInstance());
+  }
+
+  public DummyStudentStore gtClass() {
+    return new DummyStudentStore();
+  }
+
+  public ExempleImpl getExempleInstance() {
+    return new ExempleImpl();
+  }
 }
